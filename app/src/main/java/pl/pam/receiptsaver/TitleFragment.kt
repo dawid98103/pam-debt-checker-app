4e6b951f48506f2837696d35c1c4952857e51f81
dawid98103 <dawid.kobylarz@protonmail.com>
2021-04-17 18:50:54 +0200

    - add title fragment - add menus

---
+++ app/src/main/java/pl/pam/receiptsaver/TitleFragment.kt
@@ -0,0 +1,26 @@
+package pl.pam.receiptsaver
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import pl.pam.receiptsaver.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false)
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+}
